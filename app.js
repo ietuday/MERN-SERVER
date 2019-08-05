41376024d8a694e5c800e641c471fda007cca67c
UDAYADITYA SINGH <udayaditya.singh@gmail.com>
2019-08-05 19:44:05 +0530

    "init"

---
+++ app.js
@@ -0,0 +1,54 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const authenticate = require('./authenticate');
+const config = require('./config');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const newsRouter = require('./routes/news');
+// const readItemsRouter = require('./routes/readitems');
+
+const url = config.mongoUrl;
+const connect = mongoose.connect(url, {useNewUrlParser: true});
+
+connect.then((db) => {
+  console.log('Connected correctly to server');
+}, (err) => { console.log(err); });
+
+let app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(passport.initialize());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/news', newsRouter);
+// app.use('/readItems', readItemsRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('<h1>Error: ' + err.message + '</h1>');
+});
+
+module.exports = app;
